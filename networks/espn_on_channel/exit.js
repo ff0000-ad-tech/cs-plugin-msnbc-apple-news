c6b6991e24c383eeca437fe2533fd15f5fc87844
Greg <Greg@US-SMO-AL-G3420.local>
2019-08-23 14:32:14 -0700

    adds android deeplink exception to ESPN App exits

--- networks/espn_on_channel/exit.js
+++ networks/espn_on_channel/exit.js
@@ -1,25 +1,36 @@
 this.exit = function(clickTag) {
 	var clickUrl = this.appendMacro('%%CLICK_URL_UNESC%%', clickTag)
 	var cachebuster = '%%CACHEBUSTER%%'
-	if (cachebuster.search('^%%') > -1) cachebuster = new Date().getTime()
+	if (cachebuster.search('^%%') > -1) {
+		cachebuster = new Date().getTime()
+	}
 
 	switch (adParams.espnChannel) {
 		case 'ESPN_FFL_APP':
-			console.log('Network -> ESPN_FFL_APP EXIT')
-			if (navigator.userAgent.toLowerCase().indexOf('android') > -1)
+			console.log('Network -> ESPN_FFL_APP EXIT (Fantasy App)')
+			if (navigator.userAgent.toLowerCase().indexOf('android') > -1) {
 				window.parent.app.ads.clickThrough(clickUrl + '?ord=' + cachebuster)
-			else window.parent.app.ads.clickThrough(clickUrl)
+			} else {
+				window.parent.app.ads.clickThrough(clickUrl)
+			}
 			break
 
 		case 'MRAID':
-			console.log('Network -> MRAID EXIT')
-			if (clickTag.search(/^sportscenter/) > -1) mraid.open(clickTag)
-			// do not append macro to the sportscenter deeplink
-			else mraid.open(clickUrl)
+			console.log('Network -> MRAID EXIT (ESPN App)')
+			if (navigator.userAgent.toLowerCase().indexOf('android') > -1) {
+				mraid.open(clickUrl + '?ord=' + cachebuster)
+			} else {
+				// do not append macro to the sportscenter deeplink
+				if (clickTag.search(/^sportscenter/) > -1) {
+					mraid.open(clickTag)
+				} else {
+					mraid.open(clickUrl)
+				}
+			}
 			break
 
 		case 'ESPN':
-			console.log('Network -> ESPN EXIT')
+			console.log('Network -> ESPN EXIT (Dot com)')
 			window.open(clickUrl, '_blank')
 			break
 	}
