3ab2da33ee42a83f6f48176ed7b654a74315977f
Derek Miranda <derek.miranda@ff0000.com>
2020-01-21 15:58:17 -0800

    Updates test file fixtures

---
+++ test/traffic-file-fixtures/1242x699/manifest.js
@@ -0,0 +1,12 @@
+// the data MUST BE VALID JSON - see http://jsonlint.com/
+FT.manifest({
+	"filename": "index.html",
+	"width": 300,
+	"height": 250,
+	"clickTagCount": 1,
+	/*-- Red.Component.manifest_misc.start --*/
+	/*-- Red.Component.manifest_misc.end --*/
+	"hideBrowsers": ["ie8"],
+	"richLoads": [],
+	"instantAds":[]
+});
