edfe73b4889548eeb293251ced2bfc693c277400
Derek Miranda <derek.miranda@ff0000.com>
2020-01-23 16:42:33 -0800

    Updates test sizes

--- test/test-double-banner/portrait/manifest.js
+++ test/test-double-banner/portrait/manifest.js
@@ -1,8 +1,8 @@
 // the data MUST BE VALID JSON - see http://jsonlint.com/
 FT.manifest({
 	"filename": "index.html",
-	"width": 414,
-	"height": 100,
+	"width": 300,
+	"height": 250,
 	"clickTagCount": 1,
 	/*-- Red.Component.manifest_misc.start --*/
 	/*-- Red.Component.manifest_misc.end --*/
